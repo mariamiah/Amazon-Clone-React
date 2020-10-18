a6ff63f9ece503828bc792e68fee982868b461b9
mariamiah <mariah.kakande@gmail.com>
2020-10-18 13:21:34 +0300

    add header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     // BEM
     <div className="App">
-      <h1>Lets build the amazon clone</h1>
+      <Header />
     </div>
   );
 }
