1e1326dea74087d9012c76b8714ac0cc7080b0eb
mariamiah <mariah.kakande@gmail.com>
2020-10-18 07:24:16 +0300

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2u9k0tMZ1vkvbU1RzfX3Fl-YEIYN-eCA",
+    authDomain: "clone-d67b9.firebaseapp.com",
+    databaseURL: "https://clone-d67b9.firebaseio.com",
+    projectId: "clone-d67b9",
+    storageBucket: "clone-d67b9.appspot.com",
+    messagingSenderId: "1065258720771",
+    appId: "1:1065258720771:web:3f5c1a82c01b5213387836",
+    measurementId: "G-L3PQCLQ8TG"
+  };
