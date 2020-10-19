6da42e1b9cf3e7c615bed6bc5de982df24588eae
mariamiah <mariah.kakande@gmail.com>
2020-10-19 13:13:36 +0300

    set up react router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './App.css';
 import Header from './Header'
 import Home from './Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     // BEM
+    <Router>
     <div className="App">
-      <Header />
-      <Home />
+      <Switch>
+        <Route path="/">
+          <Header />
+          <Home />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
